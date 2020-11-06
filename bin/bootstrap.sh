8d105418dc66646ea622f2003e9ca7fc1129654d
Nick Phair <nickphair@gmail.com>
2020-11-05 23:39:56 -0500

    update bootstrap to not depend on homebrew

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 declare -r ANTLR_VERSION=4
 declare -r JAVA_VERSION=8
-declare -r LLVM_VERSION=10
+declare -r LLVM_MAJOR_VERSION=10
+declare -r LLVM_VERSION=${LLVM_MAJOR_VERSION}.0.0
+declare -r LLVM_MACOS_BINARY_URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-apple-darwin.tar.xz
+declare -r LLVM_MACOS_DIR=${HOME}/llvm
+
+echogreen() {
+  local green=$(tput setaf 2)
+  local reset=$(tput sgr0)
+  echo "${green}${@}${reset}"
+}
 
 echoerr() { 
-  echo "$@" 1>&2; 
+  local red=$(tput setaf 1)
+  local reset=$(tput sgr0)
+  echo "${red}${@}${reset}" 1>&2; 
 }
 
 
@@ -26,29 +37,29 @@ bootstrap_ubuntu_dependencies() {
   sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
   sudo apt -y install \
-    libllvm-$LLVM_VERSION-ocaml-dev \
-    libllvm$LLVM_VERSION \
-    llvm-$LLVM_VERSION \
-    llvm-$LLVM_VERSION-dev \
-    llvm-$LLVM_VERSION-doc \
-    llvm-$LLVM_VERSION-examples \
-    llvm-$LLVM_VERSION-runtime
+    libllvm-$LLVM_MAJOR_VERSION-ocaml-dev \
+    libllvm$LLVM_MAJOR_VERSION \
+    llvm-$LLVM_MAJOR_VERSION \
+    llvm-$LLVM_MAJOR_VERSION-dev \
+    llvm-$LLVM_MAJOR_VERSION-doc \
+    llvm-$LLVM_MAJOR_VERSION-examples \
+    llvm-$LLVM_MAJOR_VERSION-runtime
 
   sudo apt -y install \
-    clang-$LLVM_VERSION \
-    clang-tools-$LLVM_VERSION \
-    clang-$LLVM_VERSION-doc \
-    libclang-common-$LLVM_VERSION-dev \
-    libclang-$LLVM_VERSION-dev \
-    libclang1-$LLVM_VERSION \
-    clang-format-$LLVM_VERSION \
-    python3-clang-$LLVM_VERSION \
-    clangd-$LLVM_VERSION
+    clang-$LLVM_MAJOR_VERSION \
+    clang-tools-$LLVM_MAJOR_VERSION \
+    clang-$LLVM_MAJOR_VERSION-doc \
+    libclang-common-$LLVM_MAJOR_VERSION-dev \
+    libclang-$LLVM_MAJOR_VERSION-dev \
+    libclang1-$LLVM_MAJOR_VERSION \
+    clang-format-$LLVM_MAJOR_VERSION \
+    python3-clang-$LLVM_MAJOR_VERSION \
+    clangd-$LLVM_MAJOR_VERSION
 }
 
 
 bootstrap_ubuntu_env() {
-  echo export TIPCLANG=$(which clang-$LLVM_VERSION) >> ~/.bashrc
+  echo export TIPCLANG=$(which clang-$LLVM_MAJOR_VERSION) >> ~/.bashrc
 }
 
 
@@ -69,8 +80,11 @@ bootstrap_linux() {
 }
 
 bootstrap_mac_env() {
-  echo export LLVM_DIR=$(brew --prefix llvm@$LLVM_VERSION)/lib/cmake >> ~/.bashrc
-  echo export TIPCLANG=$(brew --prefix llvm@$LLVM_VERSION)/bin/clang >> ~/.bashrc
+  local LLVM_INSTALL_DIR=${LLVM_MACOS_DIR}/$(basename ${LLVM_MACOS_BINARY_URL} .tar.xz)
+  echo export CC=${LLVM_INSTALL_DIR}/bin/clang >> ~/.bashrc
+  echo export CXX=${LLVM_INSTALL_DIR}/bin/clang++ >> ~/.bashrc
+  echo export LLVM_DIR=${LLVM_INSTALL_DIR}/lib/cmake >> ~/.bashrc
+  echo export TIPCLANG=${LLVM_INSTALL_DIR}/bin/clang >> ~/.bashrc
 }
 
 bootstrap_mac_dependencies() {
@@ -84,9 +98,13 @@ bootstrap_mac_dependencies() {
     git \
     cmake \
     pkg-config \
-    llvm@$LLVM_VERSION \
     antlr@$ANTLR_VERSION \
     lcov
+
+  mkdir -p ${LLVM_MACOS_DIR}
+  wget ${LLVM_MACOS_BINARY_URL} --output-document ${LLVM_MACOS_DIR}/$(basename ${LLVM_MACOS_BINARY_URL})
+  tar xzf ${LLVM_MACOS_DIR}/$(basename ${LLVM_MACOS_BINARY_URL}) --cd ${LLVM_MACOS_DIR}
+
 }
 
 
@@ -108,8 +126,11 @@ bootstrap() {
       exit 1
       ;;
   esac
-  echo bashrc has been updated - be sure to source the file or restart your shell.
 
+  echo 
+  echo --------------------------------------------------------------------------------
+  echo * bashrc has been updated - be sure to source the file or restart your shell.  *
+  echo --------------------------------------------------------------------------------
 }
 
 bootstrap
