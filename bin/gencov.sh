d334c2754d6f3656585981f111c028522864dcc0
Nick Phair <nickphair@gmail.com>
2021-07-05 22:20:11 -0400

    try complicated build

--- bin/gencov.sh
+++ bin/gencov.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-declare -r ROOT_DIR=${TRAVIS_BUILD_DIR:-$(git rev-parse --show-toplevel)}
+declare -r ROOT_DIR=${GITHUB_WORKSPACE:-$(git rev-parse --show-toplevel)}
 
 pushd $ROOT_DIR
 lcov --capture --directory build -output-file coverage.info
